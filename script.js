95f6c89ac2119fea384dd399ea3cf1c7b651c4c2
Raj Chaurasiya <rc808129@gmail.com>
2025-11-29 18:34:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
+
+let input1 = document.getElementById('block_id')
+	let input2 = document.getElementById('colour_id')
+
+input1.addEventListener('keydown', function(e){
+		if(e.key === "Enter"){
+			input2.focus();
+		}
+	})
+let values;
 document.getElementById('change_button').addEventListener('click', function() {
+	
     let blockId = document.getElementById('block_id').value;
     let color = document.getElementById('colour_id').value;
-
-    let items = document.querySelectorAll('.grid-item');
-    items.forEach(item => {
-        item.style.backgroundColor = 'transparent';
-    });
+	
+    if(values){
+		values.style.backgroundColor = 'transparent';
+	}
+	values = document.getElementById(blockId)
+    // let items = document.querySelectorAll('.grid-item');
+    // items.forEach(item => {
+    //     item.style.backgroundColor = 'transparent';
+    // });
 
     document.getElementById(blockId).style.backgroundColor = color;
 });
 
 document.getElementById('reset_button').addEventListener('click', function() {
     let items = document.querySelectorAll('.grid-item');
-    items.forEach(item => {
-        item.style.backgroundColor = 'transparent';
-    });
+
+    // items.forEach(item => {
+    //     item.style.backgroundColor = 'transparent';
+    // });
+	values.style.backgroundColor = 'transparent';
+	
+	 document.getElementById('block_id').value = "";
+    document.getElementById('colour_id').value = "";
 });
